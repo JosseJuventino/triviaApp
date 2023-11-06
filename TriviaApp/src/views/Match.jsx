a827deaabbba9b67d7aa3edfca729f9ac939a7ae
Fernando0215 <00103722@uca.edu.sv>
2023-11-06 02:01:06 -0600

    ls boofeado

--- TriviaApp/src/views/Match.jsx
+++ TriviaApp/src/views/Match.jsx
@@ -1,16 +1,23 @@
-
+import { spread } from "axios";
+import { useState } from "react";
 
 function Match(){
 
     let _LGTB = JSON.parse(localStorage.getItem ("question"));
+    const[quesNum, setquesNum] = useState(0);
+    let newArray = [_LGTB[0].results[quesNum].incorrect_answers]
+    
 
-    console.log (_LGTB);
 
     return(
         <>
         <div>
             <h1>Match</h1>
-            
+            <h3><div dangerouslySetInnerHTML={{__html: _LGTB[0].results[quesNum].question}} /></h3>
+
+            <button></button>
+            <button></button>
+            <button></button>
         </div>
         </>
 
